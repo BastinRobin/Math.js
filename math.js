90785d9bb3157936cd16e71ed240b4e3574326b7
Bastin Robin <bastinrobins@gmail.com>
2013-06-03 14:16:25 +0530

    Create math.js
    Created the math.js file but need more enhancement its not active but will be soon

---
+++ math.js
@@ -0,0 +1,33 @@
+//Math Arithmetic programming using javascript
+
+//Arithmetic Operation
+//Function for addition
+function add(a, b){
+  var a = this.a;
+	var b = this.b;
+	return a + b;
+}
+//Function for subtraction
+function sub(a, b){
+	var a = this.a;
+	var b = this.b;
+	return a - b;
+}
+//Function for multiplication
+function mul(a, b){
+	var a = this.a;
+	var b = this.b;
+	return a * b;
+}
+//Function for division
+function div(a, b){
+	var a = this.a;
+	var b = this.b;
+	return a / b;
+}
+//Function for divisibility
+function rem(a, b){
+	var a = this.a;
+	var b = this.b;
+	return a % b;
+}
